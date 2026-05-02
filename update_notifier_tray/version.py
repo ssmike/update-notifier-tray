56f7225bb0ff4e1063c448a8775d9f068ce27649
Mikhail Surin <surinmike@gmail.com>
2026-05-02 15:36:24 +0200

    upver

--- update_notifier_tray/version.py
+++ update_notifier_tray/version.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2014 Sebastian Pipping <sebastian@pipping.org>
 # Licensed under GPL v3 or later
 
-VERSION = (1, 3)
+VERSION = (1, 4)
 VERSION_STR = '.'.join([str(e) for e in VERSION])
